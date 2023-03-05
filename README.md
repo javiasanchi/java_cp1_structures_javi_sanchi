4fd1dea4a0275a920c28a938b0d19888da4d9410
Javier <javi.ssweb@gmail.com>
2023-03-05 12:40:54 +0100

    Caso práctico 1 - Aplicación consola Java SE y estructuras de datos - Estructura básica de clase

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Este proyecto voy a intentar crearlo lo más independiente del ejemplo
 El objetivo que tengo es comprender la funcionalidad del programa y como se vinculan las diferentes partes
 Formato de creación de proyecto siguiendo indicaciones caso práctico
 1. Creo proyecto https://github.com/javiasanchi/java_cp1_structures_javi_sanchi
-2. Creo clases Book y Author
+2. Creo clases Book, Author y TypeBook (enum)
+3. Creo constructores, getters, setter y toString con Generate
+4. 
--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class Book {
 
+    // atributos Book
     private Long id;
     private String title;
     private String synopsis;
@@ -17,7 +18,136 @@ public class Book {
     private LocalDate publicDate;
     private LocalDate finalSale;
 
+    // constructores Book
 
+    public Book (){}
 
+    public Book(Long id, String title, String synopsis, Author author, Integer numPages, String isbn, String language, Boolean available, Editorial editorial, Double price, LocalDate publicDate, LocalDate finalSale) {
+        this.id = id;
+        this.title = title;
+        this.synopsis = synopsis;
+        this.author = author;
+        this.numPages = numPages;
+        this.isbn = isbn;
+        this.language = language;
+        this.available = available;
+        this.editorial = editorial;
+        this.price = price;
+        this.publicDate = publicDate;
+        this.finalSale = finalSale;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Integer getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(Integer numPages) {
+        this.numPages = numPages;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public Editorial getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(Editorial editorial) {
+        this.editorial = editorial;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getPublicDate() {
+        return publicDate;
+    }
+
+    public void setPublicDate(LocalDate publicDate) {
+        this.publicDate = publicDate;
+    }
+
+    public LocalDate getFinalSale() {
+        return finalSale;
+    }
+
+    public void setFinalSale(LocalDate finalSale) {
+        this.finalSale = finalSale;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", synopsis='" + synopsis + '\'' +
+                ", author=" + author +
+                ", numPages=" + numPages +
+                ", isbn='" + isbn + '\'' +
+                ", language='" + language + '\'' +
+                ", available=" + available +
+                ", editorial=" + editorial +
+                ", price=" + price +
+                ", publicDate=" + publicDate +
+                ", finalSale=" + finalSale +
+                '}';
+    }
 }
